e7f8133dd71594e0a578609b974c9b0f78bb8fb3
yumamur <pccartman@gmail.com>
2023-10-26 19:11:09 +0300

    getenv

--- src/shell/ft_getenv.c
+++ src/shell/ft_getenv.c
@@ -10,25 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "shellft.h"
+#include "../../include/shellft.h"
 
 static t_c_char	*ft_getenv_2(t_c_char *envp[], t_c_char *name)
 {
-	t_c_char	**ptenv;
 	size_t		len;
 	t_ushort	name_start;
 	t_ushort	ptenv_start;
 
-	ptenv = envp;
 	name_start = (name[0] * 256) + name[1];
 	len = ft_strlen(name);
-	while (*ptenv)
+	while (*envp)
 	{
-		ptenv_start = ((*ptenv)[0] * 256) + (*ptenv)[1];
-		if (ptenv_start == name_start && (*ptenv)[len] == '='
-			&& !ft_strncmp(*ptenv + 2, name + 2, len - 2))
-			return (&(*ptenv)[len + 1]);
-		ptenv++;
+		ptenv_start = ((*envp)[0] * 256) + (*envp)[1];
+		if (ptenv_start == name_start && (*envp)[len] == '='
+			&& !ft_strncmp(*envp + 2, name + 2, len - 2))
+			return (&(*envp)[len + 1]);
+		envp++;
 	}
 	return (NULL);
 }
